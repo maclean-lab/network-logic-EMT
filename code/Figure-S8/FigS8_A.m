a5411508fe25f16e5f50914a81166cf8566af57c
AnupamDey65 <anupamdey65@gmail.com>
2025-04-24 21:15:39 -0700

    Add files via upload

---
+++ code/Figure-S8/FigS8_A.m
@@ -0,0 +1,67 @@
+
+
+clear
+clc
+
+load param_ThrePert_SA0SB0_25pc_AND_mRNA.mat
+load param_AND_RandParVal_mRNA.mat
+
+SNB_OG = SNB(nrr,:);    % SN points for unperturbed models
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Analysis of the SN points for the direct perturbations
+
+diff_OG_dir = SNB_OG-SNB_SB0ltSA0;
+
+pc_moved_dir = diff_OG_dir.*100./SNB_OG;
+
+avg_pc_moved_dir = mean(pc_moved_dir);
+
+
+std_err_dir = std(pc_moved_dir) ./ sqrt(size(pc_moved_dir, 1));
+
+custom_colors = ['r', 'g', 'b','yellow'];
+
+figure(2)
+subplot(3,2,5)
+bar(avg_pc_moved_dir, 'FaceColor', 'flat');
+
+for i = 1:numel(avg_pc_moved_dir)
+    c = custom_colors(i);
+    barh(i, avg_pc_moved_dir(i), 'FaceColor', c);
+    hold on;
+    errorbar(avg_pc_moved_dir(i), i, std_err_dir(i), 'horizontal', 'Color', 'k');
+end
+
+hold off;
+
+
+% Reverse the x-axis
+set(gca, 'XDir', 'reverse');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Analysis of the SN points for the indirect perturbations
+
+diff_OG_ind = SNB_OG-SNB_SA0ltSB0;
+
+pc_moved_ind = diff_OG_ind.*100./SNB_OG;
+
+avg_pc_moved_ind = mean(pc_moved_ind);
+
+
+std_err_ind = std(pc_moved_ind) ./ sqrt(size(pc_moved_ind, 1));
+
+subplot(3,2,6)
+bar(avg_pc_moved_ind, 'FaceColor', 'flat');
+
+for i = 1:numel(avg_pc_moved_ind)
+    c = custom_colors(i);
+    barh(i, avg_pc_moved_ind(i), 'FaceColor', c);
+    hold on;
+    errorbar(avg_pc_moved_ind(i), i, std_err_ind(i), 'horizontal', 'Color', 'k');
+end
+
+hold off;
+
+% Reverse the x-axis
+set(gca, 'XDir', 'reverse');
