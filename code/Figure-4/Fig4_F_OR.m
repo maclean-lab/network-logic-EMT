2069345cc6718d1e919db8057ca0fb4402e7f889
AnupamDey65 <anupamdey65@gmail.com>
2025-04-24 20:48:45 -0700

    Add files via upload

---
+++ code/Figure-4/Fig4_F_OR.m
@@ -0,0 +1,105 @@
+clear
+clc
+
+load param_ThrePert_SA0SB0_25pc_OR.mat
+load param_SameSA0SB0_OR.mat
+
+param_unp = param_OR(:,nrr);
+%param_thre = param_ThrePert_OR(:,nrr);
+
+
+unp=tri_types(nrr,:);
+ind=tri_type_SA0ltSB0;
+dir=tri_type_SB0ltSA0;
+
+[unp_sorted,idx] = sort(unp);
+dir_sorted = dir(idx);
+ind_sorted = ind(idx);
+
+SN_DirSorted = SNB_SB0ltSA0(idx,:);
+SN_IndSorted = SNB_SA0ltSB0(idx,:);
+
+SNB_Unp = SNB(nrr,:);
+SNB_Dir = SNB_SB0ltSA0;
+SNB_Ind = SNB_SA0ltSB0;
+
+% Type 4 (1U1D): SN1>SN3>SN2>SN4
+% Type 2 (1U2D): SN1>SN3>SN4>SN2
+% Type 3 (2U1D): SN3>SN1>SN2>SN4
+% Type 1 (2U2D): SN3>SN1>SN4>SN2
+
+nr = length(nrr);
+
+for ik = 1:nr
+
+    if SNB_Unp(ik,1)>SNB_Unp(ik,3) && SNB_Unp(ik,3)>SNB_Unp(ik,2) && SNB_Unp(ik,2)>SNB_Unp(ik,4)
+        triType_Unp(ik,1) = 4;
+    elseif SNB_Unp(ik,1)>SNB_Unp(ik,3) && SNB_Unp(ik,3)>SNB_Unp(ik,4) && SNB_Unp(ik,4)>SNB_Unp(ik,2)
+        triType_Unp(ik,1) = 2;
+    elseif SNB_Unp(ik,3)>SNB_Unp(ik,1) && SNB_Unp(ik,1)>SNB_Unp(ik,2) && SNB_Unp(ik,2)>SNB_Unp(ik,4)
+        triType_Unp(ik,1) = 3;
+    elseif SNB_Unp(ik,3)>SNB_Unp(ik,1) && SNB_Unp(ik,1)>SNB_Unp(ik,4) && SNB_Unp(ik,4)>SNB_Unp(ik,2)
+        triType_Unp(ik,1) = 1;
+    end
+
+    if SNB_Dir(ik,1)>SNB_Dir(ik,3) && SNB_Dir(ik,3)>SNB_Dir(ik,2) && SNB_Dir(ik,2)>SNB_Dir(ik,4)
+        triType_Dir(ik,1) = 4;
+    elseif SNB_Dir(ik,1)>SNB_Dir(ik,3) && SNB_Dir(ik,3)>SNB_Dir(ik,4) && SNB_Dir(ik,4)>SNB_Dir(ik,2)
+        triType_Dir(ik,1) = 2;
+    elseif SNB_Dir(ik,3)>SNB_Dir(ik,1) && SNB_Dir(ik,1)>SNB_Dir(ik,2) && SNB_Dir(ik,2)>SNB_Dir(ik,4)
+        triType_Dir(ik,1) = 3;
+    elseif SNB_Dir(ik,3)>SNB_Dir(ik,1) && SNB_Dir(ik,1)>SNB_Dir(ik,4) && SNB_Dir(ik,4)>SNB_Dir(ik,2)
+        triType_Dir(ik,1) = 1;
+    end
+
+    if SNB_Ind(ik,1)>SNB_Ind(ik,3) && SNB_Ind(ik,3)>SNB_Ind(ik,2) && SNB_Ind(ik,2)>SNB_Ind(ik,4)
+        triType_Ind(ik,1) = 4;
+    elseif SNB_Ind(ik,1)>SNB_Ind(ik,3) && SNB_Ind(ik,3)>SNB_Ind(ik,4) && SNB_Ind(ik,4)>SNB_Ind(ik,2)
+        triType_Ind(ik,1) = 2;
+    elseif SNB_Ind(ik,3)>SNB_Ind(ik,1) && SNB_Ind(ik,1)>SNB_Ind(ik,2) && SNB_Ind(ik,2)>SNB_Ind(ik,4)
+        triType_Ind(ik,1) = 3;
+    elseif SNB_Ind(ik,3)>SNB_Ind(ik,1) && SNB_Ind(ik,1)>SNB_Ind(ik,4) && SNB_Ind(ik,4)>SNB_Ind(ik,2)
+        triType_Ind(ik,1) = 1;
+    end
+
+
+end
+
+diff_ind_SN31 = SNB_Ind(:,3)-SNB_Ind(:,1);
+diff_ind_SN42 = SNB_Ind(:,4)-SNB_Ind(:,2);
+diff_ind = diff_ind_SN31+diff_ind_SN42;
+
+diff_dir_SN31 = SNB_Dir(:,3)-SNB_Dir(:,1);
+diff_dir_SN42 = SNB_Dir(:,4)-SNB_Dir(:,2);
+diff_dir = diff_dir_SN31+diff_dir_SN42;
+
+
+
+
+subplot(4,4,3)
+plot(diff_ind_SN31,diff_dir_SN31,'o')
+hold on
+plot([-250,250],[-250,250])
+plot([-250,250],[0,0])
+plot([0,0],[-250,250])
+xlabel('Ind (SN3-SN1)')
+ylabel('Dir (SN3-SN1)')
+
+percent_ind_EMT = (length(find(diff_ind_SN31>diff_dir_SN31))/length(diff_ind_SN31))*100
+percent_ind_MET = (length(find(diff_ind_SN42>diff_dir_SN42))/length(diff_ind_SN42))*100
+
+subplot(4,4,4)
+plot(diff_ind_SN42,diff_dir_SN42,'o')
+hold on
+plot([-250,250],[-250,250])
+plot([-250,250],[0,0])
+plot([0,0],[-250,250])
+xlabel('Ind (SN4-SN2)')
+ylabel('Dir (SN4-SN2)')
+
+
+
+% Convert data into a matrix
+data = [unp_sorted';dir_sorted';ind_sorted'];
+
+
